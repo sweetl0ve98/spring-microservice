42948be2ef72a01e99c02659281d263afdff4e04
Ha Nguyen Quang <loveparadise98@gmail.com>
2019-10-08 11:40:07 +0700

    first commit

---
+++ gallery-service/src/main/java/com/nttdata/microservice/gallery_service/GalleryApp.java
@@ -0,0 +1,28 @@
+package com.nttdata.microservice.gallery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GalleryApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GalleryApp.class, args);
+    }
+}
+
+@Configuration
+class RestTemplateConfig {
+	
+	// Create a bean for restTemplate to call services
+	@Bean
+	@LoadBalanced		// Load balance between service instances running at different ports.
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
+	}
+}
